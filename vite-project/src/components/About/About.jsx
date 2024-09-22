298b6d9648cbdacd6fbd43df0f3cc763a1ae0fa9
ShamindaKanchana <msk60310910@gmail.com>
2024-09-22 23:09:22 +0530

    SOme updates in about

--- vite-project/src/components/About/About.jsx
+++ vite-project/src/components/About/About.jsx
@@ -6,13 +6,12 @@ import { getImageUrl } from "../../utils";
 export const About = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
+      <div className={styles.content}><br /><br /><br />
         <h1 className={styles.title}>Hi, I'm Shaminda</h1>
       <p className={styles.description}> 
-        Aspiring to become a versatile data professional, I am driven by a passion for both data science and data analysis. I am
-dedicated to continuous learning and staying up-to-date with the latest advancements in the field. My goal is to leverage my
-analytical skills to extract valuable insights from raw data, transforming it into actionable intelligence that drives innovative
-solutions
+                           Aspiring to become a versatile data professional, I am driven by a passion for both data science and data analysis. I am dedicated to continuous learning and staying up-to-date with the latest advancements in the field. My goal is to leverage my analytical skills to extract valuable insights from raw data, transforming it into actionable intelligence that drives innovative solutions.<br></br><br />
+
+        Currently pursuing a <b>BSc Honours in Computer Science at the University of Jaffna</b>, I am an undergraduate with a strong commitment to growing my knowledge and experience in the domain of data science.
         </p>
         <a href="mailto:msk60310910@gmail.com" className={styles.contactBtn}>
           Contact Me
