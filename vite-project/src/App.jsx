fcae5953cce3ae45b72dbbb478578ebca193a23c
ShamindaKanchana <msk60310910@gmail.com>
2024-10-22 18:43:25 +0530

    Some modifications

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import { Certificate } from "./components/Certificate/Certificate";
 import { Contact } from "./components/Contact/Contact";
-
+import { Badges } from "./components/Badges/Badges";
 import { About } from "./components/About/About";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <About />
       <Certificate />
+      <Badges />
       <Projects />
       <Contact />
     </div>
